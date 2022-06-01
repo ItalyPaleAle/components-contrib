a50564cb8d2a68806f571f29f2d7ff8f9d065802
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2022-06-01 00:16:32 +0000

    Kafka: Correctly unsubscribing when context is canceled
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pubsub/kafka/kafka.go
+++ pubsub/kafka/kafka.go
@@ -23,16 +23,34 @@ import (
 )
 
 type PubSub struct {
-	kafka *kafka.Kafka
+	kafka           *kafka.Kafka
+	logger          logger.Logger
+	subscribeCtx    context.Context
+	subscribeCancel context.CancelFunc
 }
 
 func (p *PubSub) Init(metadata pubsub.Metadata) error {
+	p.subscribeCtx, p.subscribeCancel = context.WithCancel(context.Background())
+
 	return p.kafka.Init(metadata.Properties)
 }
 
 func (p *PubSub) Subscribe(ctx context.Context, req pubsub.SubscribeRequest, handler pubsub.Handler) error {
 	p.kafka.AddTopicHandler(req.Topic, adaptHandler(handler))
-	return p.kafka.Subscribe(ctx)
+
+	go func() {
+		// Wait for context cancelation
+		<-ctx.Done()
+
+		// Remove the topic handler and restart the subscriber
+		p.kafka.RemoveTopicHandler(req.Topic)
+		err := p.kafka.Subscribe(p.subscribeCtx)
+		if err != nil {
+			p.logger.Errorf("kafka pubsub: error re-subscribing: %v", err)
+		}
+	}()
+
+	return p.kafka.Subscribe(p.subscribeCtx)
 }
 
 // NewKafka returns a new kafka pubsub instance.
@@ -41,7 +59,8 @@ func NewKafka(logger logger.Logger) pubsub.PubSub {
 	// in kafka pubsub component, enable consumer retry by default
 	k.DefaultConsumeRetryEnabled = true
 	return &PubSub{
-		kafka: k,
+		kafka:  k,
+		logger: logger,
 	}
 }
 
@@ -51,6 +70,7 @@ func (p *PubSub) Publish(req *pubsub.PublishRequest) error {
 }
 
 func (p *PubSub) Close() (err error) {
+	p.subscribeCancel()
 	return p.kafka.Close()
 }
 
